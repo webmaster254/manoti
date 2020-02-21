d7af9f561ed9c3a39d8239296cecf846867d2ff9
joseph <joseph2800@gmail.com>
2020-02-21 12:45:31 +0300

    Designed dashboard layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>LaraStart | Starter Laravel Application</title>
+  <title>Manoti | Microfinance Application</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/css/app.css">
 </head>
@@ -46,7 +46,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <a href="index3.html" class="brand-link">
       <img src="./img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">Lara Start</span>
+      <span class="brand-text font-weight-light">Manoti</span>
     </a>
 
     <!-- Sidebar -->
@@ -71,29 +71,83 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
 
             <li class="nav-item">
-            <router-link to="/dashboard" class="nav-link">
-                <i class="nav-icon fas fa-tachometer-alt blue"></i>
+            <router-link to="/home" class="nav-link">
+                <i class="nav-icon fas fa-home blue"></i>
                 <p>
-                Dashboard
+                Home
 
                 </p>
             </router-link>
             </li>
 
+
+            <li class="nav-item">
+            <router-link to="/customers" class="nav-link">
+                <i class="nav-icon fas fa-user blue"></i>
+                <p>
+                Customer Profile
+
+                </p>
+            </router-link>
+            </li>
+
+            
             @can('isAdmin')
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
+               <a href="#" class="nav-link">
               <i class="nav-icon fa fa-cog green"></i>
               <p>
-                Management
+                Action
                 <i class="right fa fa-angle-left"></i>
               </p>
-            </a>
+                 </a>
             <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <router-link to="/addCustomer" class="nav-link">
+                  <i class="fas fa-user-plus nav-icon"></i>
+                  <p>Add Customer</p>
+                </router-link>
+              </li>
               <li class="nav-item">
-                <router-link to="/users" class="nav-link">
+                <router-link to="/finances" class="nav-link">
                   <i class="fas fa-users nav-icon"></i>
-                  <p>Users</p>
+                  <p>Manage Finances</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/pendingLoans" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Pending Loans</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/pendingDisbursment" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Pending Disbursments</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/messenger" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Messenger</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/employees" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Employees</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/adminRequest" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Admin Requests</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/ussd" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>USSD Activity</p>
                 </router-link>
               </li>
 
@@ -101,10 +155,187 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
+            <router-link to="/dashboard" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt blue"></i>
+                <p>
+                Dashboard
+
+                </p>
+            </router-link>
+          </li>
+
+            <li class="nav-item">
+            <router-link to="/payroll" class="nav-link">
+                <i class="nav-icon fas fa-money-bill-alt blue"></i>
+                <p>
+                Payroll
+
+                </p>
+            </router-link>
+            </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-cog green"></i>
+              <p>
+                Reports
+                <i class="right fa fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <router-link to="/customersView" class="nav-link">
+                  <i class="fas fa-user-plus nav-icon"></i>
+                  <p> Customers</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/customerLoans" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Customer Loans</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/payments" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Payments</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/overdueInstallments" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Overdue Installments</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/DefaultedLoans" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Defaulted Loans</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/portfolio" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>BDO portfolio</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/overpayments" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>over payments</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/debitRecovery" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>debit Recovery</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/BooksofAccounts" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Books of Accounts</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/employees" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Employees</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/payroll" class="nav-link">
+                  <i class="fas fa fa-money-bill-alt nav-icon"></i>
+                  <p>Payroll</p>
+                </router-link>
+              </li>
+
+            </ul>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-money-bill-alt green"></i>
+              <p>
+                Transactions
+                <i class="right fa fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <router-link to="/disbursment" class="nav-link">
+                  <i class="fas fa-user-plus nav-icon"></i>
+                  <p> Disbursment</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/charges" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Charges</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/credits" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Credits</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/Debits" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Debits</p>
+                </router-link>
+              </li>
+              </ul>
+             </li>
+
+            <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-calendar-alt green"></i>
+              <p>
+                Calendars
+                <i class="right fa fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <router-link to="/disbursmentcalendar" class="nav-link">
+                  <i class="fas fa-user-plus nav-icon"></i>
+                  <p> Disbursment</p>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/collection" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Collection</p>
+                </router-link>
+              </li>
+              </ul>
+              </li>
+
+              <li class="nav-item has-treeview">
+               <a href="#" class="nav-link">
+              <i class="nav-icon fa fa-cog green"></i>
+              <p>
+                Targets
+                <i class="right fa fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <router-link to="/disbursmentcalendar" class="nav-link">
+                  <i class="fas fa-dot-circle nav-icon"></i>
+                  <p> Disbursment Targets</p>
+                </router-link>
+              </li>
+              </ul>
+              </li>
+
+          <li class="nav-item">
                 <router-link to="/developer" class="nav-link">
                     <i class="nav-icon fas fa-cogs"></i>
                     <p>
-                        Developer
+                        Administration
                     </p>
                 </router-link>
          </li>
@@ -131,7 +362,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                  @csrf
              </form>
-        </li>
+         </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
@@ -159,10 +390,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2020 <a href="#">Manoti</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->
